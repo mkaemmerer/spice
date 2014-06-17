415660ffd828c7ca799bcbb5a51733d227467fbe
Matt Kaemmerer <mattk@spiceworks.com>
2014-06-17 12:27:13 -0500

    Cleaned up examples directory

--- examples/scripts/test.js
+++ examples/scripts/test.js
@@ -1,46 +1,44 @@
 (function($spice, $){
-	'use strict';
+  'use strict';
 
-	var body = $('body');
-	$spice(body[0])
-		//Header section
-		.section()
-			.div().$class('row')
-				.h1().text('Hello, World').close()
+  var body = $('body');
+  $spice(body[0])
+    //Header section
+    .section().$class('ui page grid')
+      .div().$class('column')
+        .h1().text('Tests').close()
 
+        //Each with vanilla array
+        .h3().text('Each with array').close()
+        .each([1,2,3])
+          .p().text(function(d){ return d; }).close()
+          .p().text(function(d){ return d; }).close()
+        .close()
 
-				// .p().text(0).close()
-				// .p().text(0).close()
+        // //Each with events
+        .h3().text('Each with events').close()
+        .each(window.Bacon.fromArray([1,2,3]).scan([], '.concat'))
+          .p().text(function(d){ return d; }).close()
+          .p().text(function(d){ return d; }).close()
+        .close()
 
-				// //Each with vanilla array
-				// // .each([1,2,3])
-				// // 	.p().text(function(d){ return d; }).close()
-				// // 	.p().text(function(d){ return d; }).close()
-				// // .close()
+        //If with vanilla boolean
+        .h3().text('If/else with boolean').close()
+        .$if(true)
+          .p().text('true').close()
+        .$else()
+          .p().text('false').close()
+        .close()
 
-				// //Each with events
-				// .each(window.Bacon.fromArray([1,2,3]).scan([], '.concat'))
-				// 	.p().text(function(d){ return d; }).close()
-				// 	.p().text(function(d){ return d; }).close()
-				// .close()
+        // If with events
+        .h3().text('If/else with events').close()
+        .$if(Bacon.repeatedly(1000, [true, false]))
+          .p().text('true').close()
+        .$else()
+          .p().text('false').close()
+        .close()
 
-				// .p().text(4).close()
-				// .p().text(4).close()
-
-				// //If with vanilla boolean
-				// .$if(true)
-				// 	.p().text('true').close()
-				// .$else()
-				// 	.p().text('false').close()
-				// .close()
-
-				.$if(Bacon.repeatedly(1000, [true, false]))
-					.p().text('true').close()
-				.$else()
-					.p().text('false').close()
-				.close()
-
-			.close()
-		.close()
+      .close()
+    .close();
 
 })(window.$spice, window.$);
